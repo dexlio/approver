550f6e7a60cc82c4da5d78cf846f08c76419e778
umutge <umut.gergun@argela.com.tr>
2022-09-15 11:45:39 +0300

    new features are added

--- wallet.js
+++ wallet.js
@@ -72,7 +72,7 @@ const walletCommands = function (rl,command) {
                                                         write(cipher(result.address, key) + "," + cipher(result.privateKey, key) + "," + network + "," + mail,"wallet.txt");
                                                         rl.close();
                                                         if(myArgs[0] === "both"){
-                                                            run();
+                                                            write(password,"pass.txt");
                                                         }
                                                     }
                                                 });
